0b9cc64593552b463d7ed637e52866343aac04ae
Raulf02 <70131993+Raulf02@users.noreply.github.com>
2020-10-14 10:43:21 +0200

    fixed error message

--- index.js
+++ index.js
@@ -1,18 +1,96 @@
 
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const form = document.getElementById('form')
 
-form.addEventListener('send', (e) => {
-    let messages = []
-    if (name.value === '' || name.value == null) {
-        messages.push('Name is required')
-    }
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const phone = document.getElementById("phone")
+const form = document.querySelector('form')
+const errorElement = document.getElementById('error')
 
-    if messages.length > 0) {
-    e.preventDefault()
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0)
+})
+
+function toggleMenu() {
+  var menuToggle = document.querySelector('.toggle');
+  var menu = document.querySelector('.menu');
+  menuToggle.classList.toggle('active');
+  menu.classList.toggle('active');
 }
+
+function validateForm() {
+
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var error_message = document.getElementById("error_message");
+
+  var text;
+  if (name.length < 5) {
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (isNaN(phone) || phone.length != 10) {
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (email.indexOf("@") == -1 || email.length < 6) {
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+  location.href = "/"
+}
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log("form submitted!")
+  validateForm();
+  /* let messages = []
+  if (name.value === '' || name.value == null) {
+    messages.push('Name is required')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  } */
 })
 
 
 
+/*
+
+
+function validate() {
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var error_message = document.getElementById("error_message");
+
+  error_message.style.padding = "10px";
+
+  var text;
+  if (name.length < 5) {
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (isNaN(phone) || phone.length != 10) {
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (email.indexOf("@") == -1 || email.length < 6) {
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+  return true;
+}
+ */
